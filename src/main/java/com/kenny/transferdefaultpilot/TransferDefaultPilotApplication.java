17af39755d32b5fa85964dc0399beda5a13762a2
kenny-kerson <kenny.kerson@gmail.com>
2023-10-08 16:41:58 +0900

    Initial Commit
    - 프로젝트 최초 생성
    - Readme 작성 시작

---
+++ src/main/java/com/kenny/transferdefaultpilot/TransferDefaultPilotApplication.java
@@ -0,0 +1,13 @@
+package com.kenny.transferdefaultpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferDefaultPilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferDefaultPilotApplication.class, args);
+    }
+
+}
